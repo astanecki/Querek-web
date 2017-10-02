f25ffb8d0c4006e8d91c7509d1bdef43e669236b
Adrian Stanecki <adrianstanecki@outlook.com>
2017-10-02 21:59:59 +0200

    [fix] hiding menu button (#5)

--- src/js/components/TopBar.jsx
+++ src/js/components/TopBar.jsx
@@ -16,6 +16,7 @@ module.exports = React.createClass({
         return (
             <AppBar
                 style={style}
+                showMenuIconButton={false}
                 title={this.props.title}/>
         );
     }
--- src/js/config/Config.jsx
+++ src/js/config/Config.jsx
@@ -1,7 +1,7 @@
 module.exports = {
 
     APP: {
-        TITLE: "{kuler}",
+        TITLE: "{Querek}",
         LOGO_URL: "https://lh3.googleusercontent.com/aHEx5b92j6eBhJPBRg1b7_rb7icGn7fzAyUPfhBgMG6uOnB16bXgCLjXkMPybI2wYg=w300"
     },
 
